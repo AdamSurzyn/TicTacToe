5b8699a457d8f20359f5ae3ffdfd88311ccef6ae
AdamSurzyn <surzyn.adam@gmail.com>
2023-02-16 14:06:33 +0100

    Dev: Making the board an array

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let gameBoard = (function(){
 
     let board = document.querySelector(".board")
+    let boardArray = 
+    [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
 
     const makeBoard = () =>{
-        for (i = 0; i < 3; i ++){
-
-            for (j = 0; j < 3; j++){
+        for (i = 0; i < boardArray.length; i ++){
+            for (j = 0; j < boardArray[i].length; j++){
                 
                 let field = document.createElement("li")
                 board.appendChild(field)
